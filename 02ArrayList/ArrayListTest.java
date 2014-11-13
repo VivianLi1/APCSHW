ec4b4222edff89fdebd426e978aaf927083161a8
Vivian Li <vivian2938@gmail.com>
2014-11-12 23:15:33 -0500

    added main

--- 02ArrayList/ArrayListTest.java
+++ 02ArrayList/ArrayListTest.java
@@ -1,10 +1,9 @@
 import java.util.*;
 
 public class ArrayListTest{
-    ArrayList M = new ArrayList();
 
-    public void collapseDuplicates(ArrayList<Integer> L){
-	for(int i = 0; i < L.size(); i++){
+    static void collapseDuplicates(ArrayList<Integer> L){
+	for(int i = 0; i < L.size()-1; i++){
 	    if(L.get(i) == L.get(i + 1)){
 		L.remove(i+1);
 	    }
@@ -12,5 +11,14 @@ public class ArrayListTest{
     }
 
     public static void main(String[]args){
+	ArrayList<Integer> K = new ArrayList<Integer>();
+	K.add(0, 1);
+	K.add(1, 2);
+	K.add(2, 2);
+	K.add(3, 3);
+	System.out.println(K.toString());
+        collapseDuplicates(K);
+	System.out.println(K.toString());
+
     }
 }
