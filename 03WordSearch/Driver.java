d511ac28c1d0672d56678208442ffd24f0afff99
Vivian Li <vivian2938@gmail.com>
2014-12-03 00:11:40 -0500

    fixed some methods and main

--- 03WordSearch/Driver.java
+++ 03WordSearch/Driver.java
@@ -4,15 +4,48 @@ import java.io.*;
 public class Driver{
     public static void main(String[]args){
 
-	WordGrid test;
+	WordGrid test = null;
 	if(args.length == 4){
-	    test = new WordGrid(Integer.parseInt(arg[0]),Integer.parseInt(arg[1]), Integer.parseInt(arg[2]));
-	try{
-	    test.loadWordsFromFile("words.txt", true);
+	    test = new WordGrid(Integer.parseInt(args[0]),Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+	    for(int i = 0; i < 4; i++){
+		if(Integer.parseInt(args[i]) % 1 != 0){
+		    System.out.println("INVALID INPUT. PLEASE USE INTEGERS.");
+		}
+	    }
+	    if(Integer.parseInt(args[3]) == 1){
+		test.loadWordsFromFile("words.txt", false);
+	    }
+	    if(Integer.parseInt(args[3] == 0)){
+		test.loadWordFromFile("words.txt", true);
+	    }
 	}
-	catch(FileNotFoundException e){
-	    System.out.println("file not found");
-	    System.exit(0);
+
+	else{
+	    if(args.length == 3){
+		test = new WordGrid(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+	    }
+
+	    if(args.length == 2){
+		test = new WordGrid(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+	    }
+
+	    if(args.length < 2){
+		test = new WordGrid(50, 50);
+		System.out.println("USING DEFAULT PUZZLE CHOICES");
+	    }
+
+	    for(int i = 0; i < args.length; i++){
+		if(Integer.parseInt(args[i]) % 1 != 0){
+		    System.out.println("INVALID INPUT. PLEASE USE INTEGERS.");
+		}
+	    }
+
+	    test.loadWordsFromFile("words.txt", false);
 	}
+
+	System.out.println( "Find these words: \n" + test.wordsInPuzzle());
+	System.out.println(test);
     }
 }
+
+
--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -15,6 +15,11 @@ public class WordGrid{
 	clear();
     }
 
+    public WordGrid(int rows, int cols, int thisSeed){
+        this(rows, cols);
+	setSeed(thisSeed);
+    }
+
     /**Set all values in the WordGrid to spaces ' '*/
     public void clear(){
 	for(int i = 0; i < data.length; i++){
@@ -51,53 +56,74 @@ public class WordGrid{
 	return b;
     }
 
-    public void addWordRandomly(String word){
+    public boolean addWordRandomly(String word){
+	boolean b = true;
 	int tries = 100;
 	for(int i = tries; i > 0; i--){	   
 	    if(addWord(word, rand.nextInt(data.length), rand.nextInt(data[0].length), rand.nextInt(3) - 1, rand.nextInt(3) - 1)){
 		break;
 	    }
+	    else{ b = (i != 0);}
 	}
+	return b;
     }
 
-    public void addListofWordsRandomly(ArrayList<String> wordlist){
+    public void addListofWords(ArrayList<String> wordlist){
 	for(int i = 0; i < wordlist.size(); i++){
-	    addWordRandomly(wordlist.get(i));
+	    if( addWordRandomly(wordlist.get(i))){
+		words.add(wordlist.get(i));
+	    }
 	}
     }
 
-    public boolean checkWord(String word, int row, int col, int dx, int dy){
+    private boolean checkWord(String word, int row, int col, int dx, int dy){
+	if (dx == 0 && dy == 0 ||
+	    row < 0 || col < 0 ||
+	    col + dy * word.length() > data[0].length || col + dy * word.length() < -1 || row + dx * word.length() > data.length || row + dx * word.length() < -1){
+	    return false;
+	}
+	for (int i = 0; i < word.length(); i++){
+	    if (data[row][col] != '-' && data[row][col] != word.charAt(i)){
+		return false;
+	    }
+	    row += dx;
+	    col += dy;
+	}
+	return true;
+    }
+    /*
+    private boolean checkWord(String word, int row, int col, int dx, int dy){
 	int l = word.length();
 	return ((dx == 0 && dy == 0) ||
 		row < 0 || col < 0 ||
 		row + dx * l >= data[0].length || row + dx * l < -1 ||
 		col + dy * l > data.length || col + dy * l < 0);
 	    }
-	    
-	
+    */	    	
 
-    public void loadWordsFromFile(String fileName, boolean fillRandomLetters)
-	throws FileNotFoundException{
-	File list = new File(fileName);
-	Scanner in = new Scanner(list);
-
-	while(in.hasNext()){
-	    String line = in.nextLine();
-	    words.add(in.next());
+    public void loadWordsFromFile(String fileName, boolean fillRandomLetters){
+	try{
+	    File list = new File(fileName);
+	    Scanner in = new Scanner(list);
+	    while(in.hasNext()){
+		words.add(in.next());
+	    }
+	    addListofWords(words);
+	    if(fillRandomLetters){
+		fillRest(fillRandomLetters);
+	    }
 	}
-
-	if(fillRandomLetters){
-	    fillRest(fillRandomLetters);
+	catch(FileNotFoundException e){
+	    System.out.println("FILE NOT FOUND!1!!");
 	}
     }
-	
-    
+	    
 
-    public void setSeed(long seed){
+    private void setSeed(long seed){
 	rand.setSeed(seed);
     }
 
-    public void fillRest(boolean fill){
+    private void fillRest(boolean fill){
 	for(int i = 0; i < data.length; i++){
 	    for(int j = 0; j < data[0].length; j++){
 		if(fill && data[i][j] == '-' ){
@@ -110,7 +136,7 @@ public class WordGrid{
     public String wordsInPuzzle(){
 	String wordList = "";
 	for(int i = 0; i < words.size(); i++){
-	    wordList = wordList + words.get(i);
+	    wordList = wordList + words.get(i) + " ";
 	    //eight words per line
 	    if(i % 8 == 0){ wordList += "\n";}
 	}
